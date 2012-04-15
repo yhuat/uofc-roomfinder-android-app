f5b142d288ed8e767de7fcf00667f9a918f7ab9b
benjamin.lautenschlaeger@gmail.com <benjamin.lautenschlaeger@gmail.com@2a04ea55-ba0c-6524-1868-2dc3a9eeda37>
2012-04-15 04:10:48 +0000

    - route segmentation - route display V1.0

--- trunk/Roomfinder Android App/src/com/uofc/roomfinder/android/activities/MapActivity.java
+++ trunk/Roomfinder Android App/src/com/uofc/roomfinder/android/activities/MapActivity.java
@@ -1,14 +1,9 @@
 package com.uofc.roomfinder.android.activities;
 
-import java.util.Arrays;
-
 import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.graphics.Color;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
@@ -17,26 +12,16 @@ import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.esri.android.map.GraphicsLayer;
-import com.esri.android.map.Layer;
-import com.esri.android.map.LocationService;
 import com.esri.android.map.MapView;
 import com.esri.android.map.ags.ArcGISDynamicMapServiceLayer;
-import com.esri.android.map.event.OnStatusChangedListener;
 import com.esri.arcgis.android.samples.attributequery.R;
-import com.esri.core.geometry.Envelope;
-import com.esri.core.geometry.GeometryEngine;
-import com.esri.core.geometry.LinearUnit;
 import com.esri.core.geometry.Point;
-import com.esri.core.geometry.SpatialReference;
-import com.esri.core.geometry.Unit;
 import com.esri.core.renderer.SimpleRenderer;
 import com.esri.core.symbol.SimpleFillSymbol;
 import com.uofc.roomfinder.android.DataModel;
+import com.uofc.roomfinder.android.map.MapDrawer;
 import com.uofc.roomfinder.android.util.Constants;
-import com.uofc.roomfinder.android.util.MyLocation;
-import com.uofc.roomfinder.android.util.MyLocation.LocationResult;
 import com.uofc.roomfinder.android.util.tasks.RoomQuery;
-import com.uofc.roomfinder.entities.routing.RoutePoint;
 
 public class MapActivity extends Activity {
 
@@ -46,23 +31,12 @@ public class MapActivity extends Activity {
 	GraphicsLayer graphicsLayer;
 	ImageButton btnSearchForm;
 	ImageButton btnArView;
+	ImageButton btnPlus;
+	ImageButton btnMinus;
 	ProgressDialog progressDialog;
 	TextView txtStatusBar;
 
 	boolean actualizing = true;
-
-	// getter & setter
-	public ProgressDialog getProgressDialog() {
-		return progressDialog;
-	}
-
-	public void setProgressDialog(ProgressDialog progressDialog) {
-		this.progressDialog = progressDialog;
-	}
-
-	public GraphicsLayer getGraphicsLayer() {
-		return graphicsLayer;
-	}
 
 	// Activity methods
 	@Override
@@ -71,42 +45,44 @@ public class MapActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.map_activity_layout);
 		mapView = (MapView) findViewById(R.id.map);
+		DataModel.getInstance().setMap(this);
 
 		// WTF?????? without this layer no layer is displayed, I have no Idea why!
 		// simple workaround: add it and set the visibility to false...
-		ArcGISDynamicMapServiceLayer beastLayer = new ArcGISDynamicMapServiceLayer("http://asebeast2.cpsc.ucalgary.ca:7000/ArcGIS/rest/services/RoomFinder/MapServer");
+		ArcGISDynamicMapServiceLayer beastLayer = new ArcGISDynamicMapServiceLayer(
+				"http://asebeast2.cpsc.ucalgary.ca:7000/ArcGIS/rest/services/RoomFinder/MapServer");
 		beastLayer.setVisible(false);
 		mapView.addLayer(beastLayer);
-		
-		//layer for rooms
+
+		// layer for rooms
 		ArcGISDynamicMapServiceLayer roomLayer = new ArcGISDynamicMapServiceLayer("http://136.159.24.32/ArcGIS/rest/services/Rooms/Rooms/MapServer");
-		roomLayer.setVisible(true);	
-		mapView.addLayer(roomLayer);	
-		
+		roomLayer.setVisible(true);
+		mapView.addLayer(roomLayer);
+
 		// add layer for buildings
 		ArcGISDynamicMapServiceLayer buildingLayer = new ArcGISDynamicMapServiceLayer(Constants.MAPSERVER_BUILDINGS_URL);
 		buildingLayer.setOpacity(0.4f);
 		mapView.addLayer(buildingLayer);
-		
+
 		// graphics layer for routes and POIs
 		graphicsLayer = new GraphicsLayer();
 		SimpleRenderer sr = new SimpleRenderer(new SimpleFillSymbol(Color.RED));
 		graphicsLayer.setRenderer(sr);
 		mapView.addLayer(graphicsLayer);
-		
+
 		mapView.setMaxResolution(10000.0);
-		
+
 		System.out.println(mapView.getMaxResolution());
 		System.out.println(mapView.getMinResolution());
-		
-		//System.out.println(mapView.getMapBoundaryExtent().getYMax() +", "+ mapView.getMapBoundaryExtent().getXMax() + " - " +  mapView.getMapBoundaryExtent().getYMin() +","+mapView.getMapBoundaryExtent().getXMin());
-		
 
-		//location listener
+		// System.out.println(mapView.getMapBoundaryExtent().getYMax() +", "+ mapView.getMapBoundaryExtent().getXMax() + " - " +
+		// mapView.getMapBoundaryExtent().getYMin() +","+mapView.getMapBoundaryExtent().getXMin());
+
+		// location listener
 		DataModel.getInstance().setCurrentPositionWGS84(new Point(-114.127575, 51.080126));
 		System.out.println(DataModel.getInstance().getCurrentPositionWGS84().getX());
 		System.out.println(DataModel.getInstance().getCurrentPositionNAD83().getX());
-		
+
 		// textBTN
 		txtStatusBar = (TextView) findViewById(R.id.txt_status);
 
@@ -124,7 +100,6 @@ public class MapActivity extends Activity {
 		// button for AR view
 		btnArView = (ImageButton) findViewById(R.id.btn_ar);
 		btnArView.setOnClickListener(new View.OnClickListener() {
-
 			@Override
 			public void onClick(View v) {
 				Intent i = new Intent();
@@ -134,95 +109,80 @@ public class MapActivity extends Activity {
 			}
 		});
 
-		
-		/*
-		mapView.setOnStatusChangedListener(new OnStatusChangedListener() {
-
-			private static final long serialVersionUID = 1L;
-
+		// button plus
+		btnPlus = (ImageButton) findViewById(R.id.btn_plus);
+		btnPlus.setOnClickListener(new View.OnClickListener() {
 			@Override
-			public void onStatusChanged(Object source, STATUS status) {
-
-				System.out.println("---Status: " + status);
-				System.out.println("----1: " + mapView.getLayer(0));
-				//System.out.println("----2: " + mapView.getLayer(1));
-				
-				//mapView.getLayer(1).setVisible(false);
-				
-				if (source == mapView && status == STATUS.INITIALIZED) {
-
-					
-					
-					// set location listener
-					LocationService ls = mapView.getLocationService();
-					ls.setAutoPan(false);
-					LocationListener locationListener = new LocationListener() {
-
-						// Zooms to the current location when first GPS fix arrives
-						public void onLocationChanged(Location loc) {
-							System.out.println("onLocationChange");
-
-							double locy = loc.getLatitude();
-							double locx = loc.getLongitude();
-							double locz = loc.getAltitude();
-							float accuracy = loc.getAccuracy();
-
-							// TODO test
-							locy = 51.080652;
-							locx = -114.129195;
-
-							// save current position in singleton
-							DataModel.getInstance().setCurrentPosition(new RoutePoint(locx, locy));
-
-							// debug print on display
-							txtStatusBar.setText("lat: " + locy + " long: " + locx + "\nalt: " + locz + " acc: " + accuracy + "m");
-
-							// actualize only if accuracy is better than 300m
-							// if (accuracy < 300) {
-							// if (actualizing) {
-							// actualizing = false;
-							Point wgspoint = new Point(locx, locy);
-							Point mapPoint = (Point) GeometryEngine.project(wgspoint, SpatialReference.create(Constants.SPARTIAL_REF_MAP),
-									mapView.getSpatialReference());
-							Unit mapUnit = mapView.getSpatialReference().getUnit();
-							//System.out.println(mapUnit);
-							double zoomWidth = Unit.convertUnits(0.08, Unit.create(LinearUnit.Code.MILE_US), mapUnit);
-							System.out.println(zoomWidth);
-							Envelope zoomExtent = new Envelope(mapPoint, zoomWidth, zoomWidth);
-							mapView.setExtent(zoomExtent);
-							// }
-						}
-
-						@Override
-						public void onProviderDisabled(String provider) {
-							// TODO Auto-generated method stub
-							System.out.println("Doh, no Location service");
-						}
-
-						@Override
-						public void onProviderEnabled(String provider) {
-							// TODO Auto-generated method stub
-							System.out.println("Yeah, we have an enabled location service");
-
-						}
-
-						@Override
-						public void onStatusChanged(String provider, int status, Bundle extras) {
-							// TODO Auto-generated method stub
-							System.out.println("location service status update");
-
-						}
-					};
-					ls.start();
-					ls.setLocationListener(locationListener);
-
-					LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
-					locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1, 1, locationListener);
-					locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 1, 1, locationListener);
-
-				}
+			public void onClick(View v) {
+				MapDrawer.displayRouteSegmentOfWaypoint(DataModel.getInstance().getRoute(), DataModel.getInstance().getRoute().getCurrentWaypoint());
+				DataModel.getInstance().getRoute().increaseCurrentWaypoint();
 			}
-		});*/
+		});
+
+		// button minus
+		btnMinus = (ImageButton) findViewById(R.id.btn_minus);
+		btnMinus.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				MapDrawer.displayRouteSegmentOfWaypoint(DataModel.getInstance().getRoute(), DataModel.getInstance().getRoute().getCurrentWaypoint());
+				//todo: if it can be decreased
+				DataModel.getInstance().getRoute().decreaseCurrentWaypoint();
+			}
+		});
+
+		/*
+		 * mapView.setOnStatusChangedListener(new OnStatusChangedListener() {
+		 * 
+		 * private static final long serialVersionUID = 1L;
+		 * 
+		 * @Override public void onStatusChanged(Object source, STATUS status) {
+		 * 
+		 * System.out.println("---Status: " + status); System.out.println("----1: " + mapView.getLayer(0)); //System.out.println("----2: " +
+		 * mapView.getLayer(1));
+		 * 
+		 * //mapView.getLayer(1).setVisible(false);
+		 * 
+		 * if (source == mapView && status == STATUS.INITIALIZED) {
+		 * 
+		 * 
+		 * 
+		 * // set location listener LocationService ls = mapView.getLocationService(); ls.setAutoPan(false); LocationListener locationListener = new
+		 * LocationListener() {
+		 * 
+		 * // Zooms to the current location when first GPS fix arrives public void onLocationChanged(Location loc) { System.out.println("onLocationChange");
+		 * 
+		 * double locy = loc.getLatitude(); double locx = loc.getLongitude(); double locz = loc.getAltitude(); float accuracy = loc.getAccuracy();
+		 * 
+		 * // TODO test locy = 51.080652; locx = -114.129195;
+		 * 
+		 * // save current position in singleton DataModel.getInstance().setCurrentPosition(new RoutePoint(locx, locy));
+		 * 
+		 * // debug print on display txtStatusBar.setText("lat: " + locy + " long: " + locx + "\nalt: " + locz + " acc: " + accuracy + "m");
+		 * 
+		 * // actualize only if accuracy is better than 300m // if (accuracy < 300) { // if (actualizing) { // actualizing = false; Point wgspoint = new
+		 * Point(locx, locy); Point mapPoint = (Point) GeometryEngine.project(wgspoint, SpatialReference.create(Constants.SPARTIAL_REF_MAP),
+		 * mapView.getSpatialReference()); Unit mapUnit = mapView.getSpatialReference().getUnit(); //System.out.println(mapUnit); double zoomWidth =
+		 * Unit.convertUnits(0.08, Unit.create(LinearUnit.Code.MILE_US), mapUnit); System.out.println(zoomWidth); Envelope zoomExtent = new Envelope(mapPoint,
+		 * zoomWidth, zoomWidth); mapView.setExtent(zoomExtent); // } }
+		 * 
+		 * @Override public void onProviderDisabled(String provider) { // TODO Auto-generated method stub System.out.println("Doh, no Location service"); }
+		 * 
+		 * @Override public void onProviderEnabled(String provider) { // TODO Auto-generated method stub
+		 * System.out.println("Yeah, we have an enabled location service");
+		 * 
+		 * }
+		 * 
+		 * @Override public void onStatusChanged(String provider, int status, Bundle extras) { // TODO Auto-generated method stub
+		 * System.out.println("location service status update");
+		 * 
+		 * } }; ls.start(); ls.setLocationListener(locationListener);
+		 * 
+		 * LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+		 * locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1, 1, locationListener);
+		 * locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 1, 1, locationListener);
+		 * 
+		 * } } });
+		 */
 	}
 
 	@Override
@@ -244,18 +204,18 @@ public class MapActivity extends Activity {
 
 		// Receiving the Data
 		Intent intent = data;
-		
-		//if no data then return
+
+		// if no data then return
 		if (data == null)
 			return;
-		
+
 		// split in room and building
 		// received data should look like ICT550
 		String receivedData = intent.getStringExtra("room");
 		String regex = "(?<=[\\w&&\\D])(?=\\d)";
 		String building = receivedData.split(regex)[0];
 		String room = receivedData.split(regex)[1];
-		
+
 		String impedance = intent.getStringExtra("impedance");
 
 		Log.e("MapScreen", "searching room: " + room + " and building: " + building + "and impedance: " + impedance);
@@ -269,4 +229,27 @@ public class MapActivity extends Activity {
 		RoomQuery asyncQuery = new RoomQuery();
 		asyncQuery.execute(queryParams);
 	}
+
+	// getter & setter
+	public ProgressDialog getProgressDialog() {
+		return progressDialog;
+	}
+
+	public void setProgressDialog(ProgressDialog progressDialog) {
+		this.progressDialog = progressDialog;
+	}
+
+	public GraphicsLayer getGraphicsLayer() {
+		return graphicsLayer;
+	}
+
+	public MapView getMapView() {
+		return mapView;
+	}
+
+	public void setMapView(MapView mapView) {
+		this.mapView = mapView;
+	}
+	
+	
 }
